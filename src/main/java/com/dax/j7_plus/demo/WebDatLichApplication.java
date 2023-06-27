830cc867ebc484d8e09a12362621fae3b5d7cdd8
ADMIN\Dell <vannvph28656@fpt.edu.vn>
2023-06-27 19:36:18 +0700

    add project

---
+++ src/main/java/com/dax/j7_plus/demo/WebDatLichApplication.java
@@ -0,0 +1,13 @@
+package com.dax.j7_plus.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDatLichApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebDatLichApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dax/j7_plus/demo/WebDatLichApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dax.j7_plus.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDatLichApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
